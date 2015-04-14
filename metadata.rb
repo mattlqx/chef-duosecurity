6e0be513f84240f2c0d1864cb4003d71641331e2
Adam Ochonicki <adam@fromonesrc.com>
2015-04-13 20:20:26 -0400

    More metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,15 @@
-name             "duosecurity"
-maintainer       "Articulate"
-maintainer_email "ops@articulate.com"
-license          "All rights reserved"
-description      "Installs/Configures duosecurity"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.2.0"
+name                "duosecurity"
+maintainer          "Articulate"
+maintainer_email    "ops@articulate.com"
+license             "MIT"
+description         "Installs/Configures duosecurity"
+long_description    IO.read(File.join(File.dirname(__FILE__), "README.md"))
+issues_url          "https://github.com/articulate/chef-duosecurity/issues"
+source_url          "https://github.com/articulate/chef-duosecurity"
+version             "0.2.0"
 
-depends "ark"
+supports            "ubuntu"
+provides            "duosecurity::default"
+recipe              "duosecurity::default", "Installs and configures login_duo"
+depends             "ark"
 
