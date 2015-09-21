8f05592ffa62ef65494432b6a97440527bf1ed16
Matt Kulka <mkulka@local-motors.com>
2015-09-21 16:36:13 -0700

    pam authentication option for login and sudo

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,6 +14,8 @@ prompts             = node["duosecurity"]["prompts"] if node["duosecurity"]["pro
 accept_env_factor   = node["duosecurity"]["accept_env_factor"] if node["duosecurity"]["accept_env_factor"]
 fallback_local_ip   = node["duosecurity"]["fallback_local_ip"] if node["duosecurity"]["fallback_local_ip"]
 https_timeout       = node["duosecurity"]["https_timeout"] if node["duosecurity"]["https_timeout"]
+use_pam             = node["duosecurity"]["use_pam"] if node["duosecurity"]["use_pam"] == "yes"
+protect_sudo        = node["duosecurity"]["protect_sudo"] if node["duosecurity"]["protect_sudo"] == "yes"
 
 include_recipe "duosecurity::#{node['duosecurity']['install_type']}"
 
@@ -25,44 +27,186 @@ directory "/etc/duo" do
 end
 
 # https://www.duosecurity.com/docs/duounix#configuration-options
-template "/etc/duo/login_duo.conf" do
-  mode "0400"
-  owner "root"
-  group "root"
-  source "login_duo.conf.erb"
-  sensitive true
-  variables(
-    integration_key: integration_key,
-    secret_key: secret_key,
-    api_hostname: api_hostname,
-    groups: groups,
-    failmode: failmode,
-    pushinfo: pushinfo,
-    http_proxy: http_proxy,
-    autopush: autopush,
-    motd: motd,
-    prompts: prompts,
-    accept_env_factor: accept_env_factor,
-    fallback_local_ip: fallback_local_ip,
-    https_timeout: https_timeout
-  )
-end
-
-# Enable login_duo and harden sshd
-# https://www.duosecurity.com/docs/duounix#3.-enable-login_duo
-sshd_directives = [
-  "ForceCommand /usr/sbin/login_duo",
-  "PermitTunnel no",
-  "AllowTcpForwarding no"
-]
-
-sshd_directives.each do |directive|
-  ruby_block "enable_login_duo_for_ssh" do
-    block do
-      fe = Chef::Util::FileEdit.new("/etc/ssh/sshd_config")
-      fe.insert_line_if_no_match(/#{directive}/, directive)
-      fe.write_file
-    end
+%w(
+  /etc/duo/login_duo.conf
+  /etc/duo/pam_duo.conf
+).each do |config|
+  template config do
+    mode "0400"
+    owner "root"
+    group "root"
+    source "login_duo.conf.erb"
+    sensitive true
+    variables(
+      integration_key: integration_key,
+      secret_key: secret_key,
+      api_hostname: api_hostname,
+      groups: groups,
+      failmode: failmode,
+      pushinfo: pushinfo,
+      http_proxy: http_proxy,
+      autopush: autopush,
+      motd: motd,
+      prompts: prompts,
+      accept_env_factor: accept_env_factor,
+      fallback_local_ip: fallback_local_ip,
+      https_timeout: https_timeout,
+      use_pam: use_pam,
+    )
   end
 end
 
+if use_pam
+  include_recipe 'pam'
+
+  node.default['pam_d']['services']['common-auth'] = {
+    'main' => {
+      'pam_unix' => {
+        'interface' => 'auth',
+        'control_flag' => 'requisite',
+        'name' => 'pam_unix.so',
+        'args' => 'nullok_secure',
+      },
+      'pam_duo' => {
+        'interface' => 'auth',
+        'control_flag' => '[success=1 default=ignore]',
+        'name' => '/lib64/security/pam_duo.so',
+      },
+      'pam_deny' => {
+        'interface' => 'auth',
+        'control_flag' => 'requisite',
+        'name' => 'pam_deny.so',
+      },
+      'pam_permit' => {
+        'interface' => 'auth',
+        'control_flag' => 'required',
+        'name' => 'pam_permit.so',
+      },
+      'pam_cap' => {
+        'interface' => 'auth',
+        'control_flag' => 'optional',
+        'name' => 'pam_cap.so',
+      },
+    }
+  }
+
+  node.default['pam_d']['services']['sshd'] = {
+    'main' => {
+      'pam_duo' => {
+        'interface' => 'auth',
+        'control_flag' => 'required',
+        'name' => '/lib64/security/pam_duo.so',
+      },
+      'pam_nologin' => {
+        'interface' => 'account',
+        'control_flag' => 'required',
+        'name' => 'pam_nologin.so',
+      },
+      'pam_selinux close' => {
+        'interface' => 'session',
+        'control_flag' => '[success=ok ignore=ignore module_unknown=ignore default=bad]',
+        'name' => 'pam_selinux.so',
+        'args' => 'close',
+      },
+      'pam_loginuid' => {
+        'interface' => 'session',
+        'control_flag' => 'required',
+        'name' => 'pam_loginuid.so',
+      },
+      'pam_keyinit' => {
+        'interface' => 'session',
+        'control_flag' => 'optional',
+        'name' => 'pam_keyinit.so',
+        'args' => 'force revoke',
+      },
+      'pam_motd dynamic' => {
+        'interface' => 'session',
+        'control_flag' => 'optional',
+        'name' => 'pam_motd.so',
+        'args' => 'motd=/run/motd.dynamic',
+      },
+      'pam_motd' => {
+        'interface' => 'session',
+        'control_flag' => 'optional',
+        'name' => 'pam_motd.so',
+        'args' => 'noupdate',
+      },
+      'pam_mail' => {
+        'interface' => 'session',
+        'control_flag' => 'optional',
+        'name' => 'pam_mail.so',
+        'args' => 'standard noenv',
+      },
+      'pam_limits' => {
+        'interface' => 'session',
+        'control_flag' => 'required',
+        'name' => 'pam_limits.so',
+      },
+      'pam_env' => {
+        'interface' => 'session',
+        'control_flag' => 'required',
+        'name' => 'pam_env.so',
+      },
+      'pam_env locale' => {
+        'interface' => 'session',
+        'control_flag' => 'required',
+        'name' => 'pam_env.so',
+        'args' => 'user_readenv=1 envfile=/etc/default/locale',
+      },
+      'pam_selinux open' => {
+        'interface' => 'session',
+        'control_flag' => '[success=ok ignore=ignore module_unknown=ignore default=bad]',
+        'name' => 'pam_selinux.so',
+        'args' => 'open',
+      },
+    },
+    'includes' => %w(
+      common-account
+      common-session
+      common-password
+    )
+  }
+
+  if protect_sudo
+    node.default['pam_d']['services']['sudo'] = {
+      'main' => {
+        'pam_env' => {
+          'interface' => 'auth',
+          'control_flag' => 'required',
+          'name' => 'pam_env.so',
+          'args' => 'readenv=1 user_readenv=0',
+        },
+        'pam_env locale' => {
+          'interface' => 'auth',
+          'control_flag' => 'required',
+          'name' => 'pam_env.so',
+          'args' => 'readenv=1 envfile=/etc/default/locale user_readenv=0',
+        },
+        'pam_duo' => {
+          'interface' => 'auth',
+          'control_flag' => 'sufficient',
+          'name' => '/lib64/security/pam_duo.so',
+        }
+      },
+      'includes' => %w(
+        common-auth
+        common-account
+        common-session-noninteractive
+      )
+    }
+  end
+end
+  
+# Enable login_duo and harden sshd
+# https://www.duosecurity.com/docs/duounix#3.-enable-login_duo
+include_recipe 'sshd'
+node.default['sshd']['sshd_config']['PermitTunnel'] = 'no'
+node.default['sshd']['sshd_config']['AllowTcpForwarding'] = 'no'
+
+if use_pam
+  node.default['sshd']['sshd_config']['UsePAM'] = 'yes'
+  node.default['sshd']['sshd_config']['UseDNS'] = 'no'
+  node.default['sshd']['sshd_config']['ChallengeResponseAuthentication'] = 'yes'
+else
+  node.default['sshd']['sshd_config']['ForceCommand'] = '/usr/sbin/login_duo'
+end
