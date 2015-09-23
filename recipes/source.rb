a201cb71016086ac89637c213d0aa2ceb826b818
Matt Kulka <mkulka@local-motors.com>
2015-09-23 14:17:33 -0700

    required flags and lib to compile duo from source with pam support

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,8 +1,17 @@
 # Install login_duo
 # https://www.duosecurity.com/docs/duounix#1.-set-up-login_duo
+configure_opts = ["--prefix=/usr"]
+
+if node['duosecurity']['use_pam'] == 'yes'
+  package 'libpam-dev' do
+    action :install
+  end
+  configure_opts << "--with-pam=/lib64/security"
+end
+
 ark "duosecurity" do
   url "https://dl.duosecurity.com/duo_unix-#{node['duosecurity']['source_version']}.tar.gz"
-  autoconf_opts ["--prefix=/usr"]
+  autoconf_opts configure_opts
   checksum node['duosecurity']['source_sha256']
   action :install_with_make
 end
