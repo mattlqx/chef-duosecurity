52d9453b80e41579bcbd5c20396ddf16c1623362
Matt Kulka <mkulka@parchment.com>
2017-11-04 20:06:27 -0700

    v 1.3.6 and reformat metadata

--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,19 @@
-name                "duosecurity"
-maintainer          "Matt Kulka"
-maintainer_email    "matt@lqx.net"
-license             "MIT"
-description         "Installs/Configures duosecurity two-factor system authentication on Ubuntu/Debian"
-long_description    IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version             "1.3.5"
+name                'duosecurity'
+maintainer          'Matt Kulka'
+maintainer_email    'matt@lqx.net'
+license             'MIT'
+description         'Installs/Configures duosecurity two-factor system authentication on Ubuntu/Debian'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             '1.3.6'
 
 chef_version        '>= 12.1'
 
-supports            "debian"
-supports            "ubuntu"
-recipe              "duosecurity::default", "Installs and configures login_duo"
-recipe              "duosecurity::package", "Installs login_duo from package"
-recipe              "duosecurity::source", "Installs login_duo from source"
-depends             "ark"
-depends             "pam"
-depends             "sshd"
-depends             "apt"
-source_url          "https://github.com/articulate/chef-duosecurity" if respond_to?(:source_url)
-issues_url          "https://github.com/mattlqx/chef-duosecurity/issues" if respond_to?(:issues_url)
+supports   'debian'
+supports   'ubuntu'
+
+depends    'ark'
+depends    'pam'
+depends    'sshd'
+depends    'apt'
+source_url 'https://github.com/mattlqx/chef-duosecurity' if respond_to?(:source_url)
+issues_url 'https://github.com/mattlqx/chef-duosecurity/issues' if respond_to?(:issues_url)
