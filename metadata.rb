7043f93ce043bcb220aa21a252f4973de2d6812f
Matt Kulka <mkulka@parchment.com>
2018-04-20 13:01:18 -0700

    bump for revert

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    'matt@lqx.net'
 license             'MIT'
 description         'Installs/Configures duosecurity two-factor system authentication on Ubuntu/Debian'
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             '1.3.6'
+version             '1.3.8'
 
 chef_version        '>= 12.1'
 
