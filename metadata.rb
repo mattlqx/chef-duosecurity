488e0ccd3a9fa1b4351f2e0368b677fc73402989
tgraskemper <thomas.graskemper@parchment.com>
2022-01-05 10:46:45 -0700

    Update with cookstyle and rubocop findings

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 name                'duosecurity'
 maintainer          'Matt Kulka'
 maintainer_email    'matt@lqx.net'
 license             'MIT'
 description         'Installs/Configures duosecurity two-factor system authentication on Ubuntu/Debian'
-long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             '2.2.0'
+version             '3.0.0'
 
-source_url 'https://github.com/mattlqx/chef-duosecurity' if respond_to?(:source_url)
-issues_url 'https://github.com/mattlqx/chef-duosecurity/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/mattlqx/chef-duosecurity'
+issues_url 'https://github.com/mattlqx/chef-duosecurity/issues'
 
-chef_version        '>= 15.3'
+chef_version '>= 15.3'
 
 supports   'debian'
 supports   'ubuntu'
